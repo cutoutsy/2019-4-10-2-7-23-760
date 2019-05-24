0404df99370554081688d8911a9fcbdc49c8d8b0
cutoutsy <cutoutsy@gmail.com>
2019-05-24 20:48:10 +0800

    finish main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var distance = inputs.distance;
+    var partTimePrice = inputs.parkTime * 0.25;
+    var price = 6 + partTimePrice;
+    if (distance < 2) {
+        ;
+    } else if (distance > 2 && distance <=8) {
+        price += (distance - 2) * 0.8;
+    } else if (distance > 8) {
+        price += 6 * 0.8 + (distance - 8) * 0.8 * 1.5;
+    } 
+    return Math.round(price);
 };
